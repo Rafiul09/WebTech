dfe53b6b76a68225e492bd4250a24bf0d3dc068a
Rafiul09 <rafiulxlr8@gmail.com>
2024-02-03 00:45:46 +0600

    changes

--- log.php
+++ log.php
@@ -1,73 +1,44 @@
-<?php 
-session_start();
-$_SESSION['message']="Invalid Credentials";
-
-$unameErr= $unpassErr="";
-?>
-<?php 
-                 
-                 if ( isset($_POST['Log'])) 
-                 {
-                   if(!empty($_POST['Uname'])&& !empty($_POST['Pass'])) 
-                   {
-                       if($_POST['Uname']== $_POST['Pass']) 
-                       {
-                           
-                           header("Location: reg.php");
-                           exit();
-                       } 
-                       elseif ($_POST['Uname']!= $_POST['Pass']) {
-                           echo "Invalid Credentials";
-                       }
-                    }
-                    if (empty($_POST['Uname'])) {
-                       $unameErr = "Name is required";
-                    }
-                    if (empty($_POST['Pass'])) {
-                       $unpassErr = "Password is required";
-                    }
-
-                   
-                
-                  
-                 
-                 }
-                # else if (isset($_POST['log']) && (isset($_POST['uname'])  isset($_POST['password'])))
-                 #{
-                  #print_r($_SESSION);
-                 #}
-               ?>
 <!DOCTYPE html>
 <title>Index</title>
 
 <body>
     <center>
-    
-        <fieldset style = "width:500px"  align="center">
-            <legend>Login</legend>
-            <form method=POST >
-            <table align="center">
-                <tr>
-                    <td>Username</td>
-                    <td><input type="text" name="Uname">
-                    <br><span class="error"><?php echo $unameErr;?></span>    
-                </td>
-                </tr>
-                <tr>
-                    <td>Password</td>
-                    <td><input type="password" name="Pass">
-                    <br><span class="error"><?php echo $unpassErr;?></span>
-                </td>
-                </tr>
-            </table>
-            <hr rowspan = "50">
-                <input type="submit" name="Log" value ="Login"  align="center">
-                <input type="submit" name="reg" value ="Signup"  align="center">
-                
-                </form>
-                
-                
-        </fieldset>
-    
+        <form method=GET action="Print.php">
+            <fieldset align="center">
+                <legend>Registration</legend>
+                <table align="center">
+                    <tr>
+                        <td>Name</td>
+                        <td><input type="text" name="Name"></td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td><input type="email" name="Em"></td>
+                    </tr>
+                    <tr>
+                        <td>Username</td>
+                        <td><input type="text" name="Uname"></td>
+                    </tr>
+                    <tr>
+                        <td> Home Address</td>
+                        <td><input type="string" name="HADD"></td>
+                    </tr>
+                    <tr>
+                        <td>Date of Birth</td>
+                        <td><input type="date" name="DOB"></td>
+                    </tr>
+
+                    <tr>
+                        <td>Password</td>
+                        <td><input type="password" name="Upass"></td>
+
+                    </tr>
+                     
+                    
+                </table>
+                <hr rowspan = "50">
+                <button align="center">Login</button>
+            </fieldset>
+        </form>
     </center>
 </body>
